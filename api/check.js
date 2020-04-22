6fa1b5518e121bf9db789a52cab50623cb59d270
bill pappas <vasilisbest@gmail.com>
2020-04-23 00:42:58 +0300

    more tests

--- api/check.js
+++ api/check.js
@@ -1,6 +1,8 @@
 module.exports = (req, res) => {
+    let status = req.key === process.env.RIGHT_KEY ? "UP" : "DOWN"
+
      res.json({
-       status: "UP"
+       status: status
      })
    //process.env.TEST_DOMAIN
 }
