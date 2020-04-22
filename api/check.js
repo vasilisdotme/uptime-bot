e1bd62a8a32cac94b6afaeddb1b050caefb22811
bill pappas <vasilisbest@gmail.com>
2020-04-23 00:26:38 +0300

    test commit

--- api/check.js
+++ api/check.js
@@ -1,1 +1,7 @@
-console.log("it works");
+module.exports = (req, res) => {
+     res.json({
+            body: req.body,
+            query: req.query,
+            cookies: req.cookies
+          })
+}
